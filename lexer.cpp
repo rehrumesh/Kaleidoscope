f6835c501f3de85b28d472f84798bfbe8dce9d6d
Rumesh Eranga <rehrumesh@hotmail.com>
2015-08-14 00:30:50 +0530

    Added fnction and binop parser

--- lexer.cpp
+++ lexer.cpp
@@ -125,3 +125,164 @@ static int getNextToken(){
 ExprAST *Error(const char *Str) { fprintf(stderr, "Error: %s\n", Str);return 0;}
 PrototypeAST *ErrorP(const char *Str) { Error(Str); return 0; }
 FunctionAST *ErrorF(const char *Str) { Error(Str); return 0; }
+
+
+static ExprAST *ParseNumberExpr(){
+	ExprAST *Result = new NumberExprAST(NumVal);
+	getNextToken();
+	return Result;
+}
+
+static ExprAST *ParseParenExpr(){
+	getNextToken();
+	ExprAST *V = ParseExpression();
+	if( !V) return 0;
+
+	if(CurTok != ')')
+		return Error("Expected ')'");
+	getNextToken();
+	return V;
+}
+
+static ExprAST *ParseIdentifierExpr(){
+	std::string IdName = IdentifierStr;
+
+	getNextToken();
+
+	if(CurTok != '(')
+		return new VariableExprAST(IdName);
+
+	getNextToken();
+	std::vector<ExprAST*> Args;
+	if(CurTok != ')'){
+		while (1) {
+			ExprAST *Arg = ParseExpression();
+			if(!Arg) return 0;
+			Args.push_pack(Arg);
+
+			if (CurTok == ')') break;
+
+      			if (CurTok != ',')
+        			return Error("Expected ')' or ',' in argument list");
+      			getNextToken();
+		}
+	}
+
+	getNextToken();
+
+	return new CallExprAST(IdName, Args);
+}
+
+static ExprAST *ParsePrimary() {
+ 	switch (CurTok) {
+  	default: return Error("unknown token when expecting an expression");
+  	case tok_identifier: return ParseIdentifierExpr();
+  	case tok_number:     return ParseNumberExpr();
+  	case '(':            return ParseParenExpr();
+  	}
+}
+
+static std::map<char, int> BinopPrecedence;
+
+static int GetTokPrecedence(){
+	if(!isascii(CurTok))
+		return -1;
+
+	int TopPrec = BinopPrecedence[CurTok];
+	if(TokPrec <= 0) return -1;
+	return TokPrec;
+}
+
+int main(){
+	BinopPrecedence['<'] = 10;
+	BinopPrecedence['+'] = 20;
+	BinopPrecedence['-'] = 20;
+	BinopPrecedence['*'] = 40;
+	//BinopPrecedence['/'] = 40;
+}
+
+static ExprAST *ParseExpression() {
+  	ExprAST *LHS = ParsePrimary();
+  	if (!LHS) return 0;
+
+  	return ParseBinOpRHS(0, LHS);
+}
+
+static ExprAST *ParseBinOpRHS(int ExprPrec, ExprAST *LHS) {
+  	while (1) {
+    		int TokPrec = GetTokPrecedence();
+    		if (TokPrec < ExprPrec)
+      			return LHS;
+      		int BinOp = CurTok;
+  		ExprAST *RHS = ParsePrimary();
+  		getNextToken();  // eat binop
+  		if (!RHS) return 0;
+
+  		int NextPrec = GetTokPrecedence();
+		if (TokPrec < NextPrec) {
+			RHS = ParseBinOpRHS(TokPrec+1, RHS);
+      			if (RHS == 0) return 0;
+		}
+    		LHS = new BinaryExprAST(BinOp, LHS, RHS);
+  	}
+}
+
+static PrototypeAST *ParsePrototype() {
+  	if (CurTok != tok_identifier)
+    		return ErrorP("Expected function name in prototype");
+
+  	std::string FnName = IdentifierStr;
+  	getNextToken();
+
+  	if (CurTok != '(')
+    		return ErrorP("Expected '(' in prototype");
+
+  	// Read the list of argument names.
+  	std::vector<std::string> ArgNames;
+  	while (getNextToken() == tok_identifier)
+    		ArgNames.push_back(IdentifierStr);
+  	if (CurTok != ')')
+    		return ErrorP("Expected ')' in prototype");
+
+  	// success.
+  	getNextToken();  // eat ')'.
+
+  	return new PrototypeAST(FnName, ArgNames);
+}
+
+static FunctionAST *ParseDefinition() {
+  	getNextToken();  // eat def.
+  	PrototypeAST *Proto = ParsePrototype();
+  	if (Proto == 0) return 0;
+
+  	if (ExprAST *E = ParseExpression())
+    		return new FunctionAST(Proto, E);
+  	return 0;
+}
+
+static PrototypeAST *ParseExtern() {
+  	getNextToken();  // eat extern.
+  	return ParsePrototype();
+}
+
+static FunctionAST *ParseTopLevelExpr() {
+  	if (ExprAST *E = ParseExpression()) {
+    		// Make an anonymous proto.
+    		PrototypeAST *Proto = new PrototypeAST("", std::vector<std::string>());
+    		return new FunctionAST(Proto, E);
+  	}
+  	return 0;
+}
+
+static void MainLoop() {
+  	while (1) {
+    		fprintf(stderr, "ready> ");
+    		switch (CurTok) {
+    		case tok_eof:    return;
+    		case ';':        getNextToken(); break;  // ignore top-level semicolons.
+    		case tok_def:    HandleDefinition(); break;
+    		case tok_extern: HandleExtern(); break;
+    		default:         HandleTopLevelExpression(); break;
+    		}
+  	}
+}
